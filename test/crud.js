dd1dbe03a6bece6bd7b1d5ab0d2351de22d9849d
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-04 15:28:58 -0700

    Create test for create read and update. Update tests test for edge cases if user does not exist.

--- test/crud.js
+++ test/crud.js
@@ -1,5 +1,42 @@
 const Crud = artifacts.require('Crud')
 
 contract('Crud', () => {
+  let crud = null;
+  before(async() => {
+    crud = await Crud.depolyed();
+  });
 
+  it('Should create a new user and read', async () => {
+    // we add 'await' so that the next line does not get executed before a user is created
+    // otherwise the next line will be executed before we finish creating the user
+    await crud.create('Ryan');
+    const user = await crud.read(1);
+    assert(user[0].toNumber() === 1);
+    assert(user[1] === 'Ryan');
+  });
+
+  it('Should update a user', async () => {
+    await crud.update(1, 'Ryyan');
+    const user = await crud.read(1);
+    assert(user[0].toNumber() === 1);
+    assert(user[1] === 'Ryyan');
+  });
+
+  it('Should NOT update a non-existing user', async () => {
+    // try block will catch an error without making the test fail
+    // everything that throws an error in a try block will be caught
+    // the catch block catches the error
+    // 'error' is the variable that will recieve the errors
+    try {
+      await crud.update(2, 'Ryyan');
+    } catch(error) {
+      // error is an object that has a message
+      assert(error.message.includes('User does not exist!'));
+      // return if test passes
+      return;
+    }
+    // a fail safe if the function in the try block does not get cause an error
+    // it will hit 'assert(false)'' and the test will fail
+    assert(false)
+  });
 });
