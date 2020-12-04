2bc593fb5f82e72d30a9aee7979569aa15ce4572
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-04 15:33:35 -0700

    Create a delete function

--- test/crud.js
+++ test/crud.js
@@ -6,6 +6,7 @@ contract('Crud', () => {
     crud = await Crud.depolyed();
   });
 
+  // async is used because we will use await
   it('Should create a new user and read', async () => {
     // we add 'await' so that the next line does not get executed before a user is created
     // otherwise the next line will be executed before we finish creating the user
@@ -37,6 +38,17 @@ contract('Crud', () => {
     }
     // a fail safe if the function in the try block does not get cause an error
     // it will hit 'assert(false)'' and the test will fail
-    assert(false)
+    assert(false);
+  });
+
+  it('Should delete a user', async () => {
+    await crud.destroy(1);
+    try {
+      await crud.read(1);
+    } catch(error) {
+      assert(error.message.includes('User does not exist!'));
+      return;
+    }
+    assert(false);
   });
 });
